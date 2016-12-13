86118a5912a6d4020ed5ad1a4453aa39cae5c427
superhin <superhin@126.com>
2016-12-13 17:41:20 +0800

    add srally V2.1.2,add crontab file and modify rally_run.sh to enable it can run with crontab

---
+++ V2.1/rally_run.sh
@@ -0,0 +1,65 @@
+#/bin/bash
+run_tasks()
+{
+export now=`date +%Y%m%d_%H%M%S`
+export report_dir=report/${now}
+export error_dir=report/${now}/error_tasks
+export start_time=`date '+%Y-%m-%d %H:%M:%S'`
+
+if [ ! -d "report" ]; then
+    mkdir report
+fi
+mkdir -p $report_dir
+mkdir -p $error_dir
+echo "----------------------------------------------------------------------------"
+echo "Rally Test Start"
+echo "----------------------------------------------------------------------------"
+total=`ls *.json | wc -l`
+for filename in `ls *.json`
+do
+    pass=0
+    fail=0
+    echo $filename
+    echo "----------------------------------------------------------------------------"
+	rally task start ${filename} 2>&1 | tee ${report_dir}/${filename%.*}.log
+	reportcmd=`cat ${report_dir}/${filename%.*}.log | grep -m1 "rally task report" | cut -d " " -f 1-5`
+	is_na = `cat ${report_dir}/${filename%.*}.log | grep 'n/a'` 2>/dev/null
+	echo $reportcmd
+	if [ -z "$reportcmd" ] || [ -n "$is_na" ]; then
+        let "fail=fail+1"
+		echo "can't find reportcmd"
+		cp $filename ${error_dir}/
+        cp ${report_dir}/${filename%.*}.log ${error_dir}/
+	else
+        let "pass=pass+1"
+		${reportcmd} ${report_dir}/${filename%.*}.html
+	fi
+	sleep 5
+done 
+export end_time=`date '+%Y-%m-%d %H:%M:%S'`
+start_stamp=`date -d "$end_time" +%s`
+end_stamp=`date -d "$start_time" +%s`
+let duration_time=($end_stamp-$start_stamp)
+let duration_h=($duration_time/3600)
+let duration_m=($duration_time%3600/60)
+let duration_s=($duration_time%60)
+echo "----------------------------------------------------------------------------"
+echo "Start Time: ${start_time}    End Time: ${end_time}"
+echo "Full Duration: ${duration_h} h  ${duration_m} m  ${duration_s} s"
+echo "----------------------------------------------------------------------------"
+
+echo "[task_info]" >rally_run.info
+echo "date=${now}" >>rally_run.info
+echo "report_dir=${report_dir}" >>rally_run.info
+echo "start_time=${start_time}" >>rally_run.info
+echo "end_time=${end_time}" >>rally_run.info
+echo "full_duration=${duration_h}h${duration_m}m${duration_s}s" >>rally_run.info
+echo "total=${total}" >>rally_run.info
+echo "pass=${pass}" >>rally_run.info
+echo "fail=${fail}" >>rally_run.info
+cp rally_run.info ${report_dir}/
+}
+run_tasks
+python collector.py
+
+
